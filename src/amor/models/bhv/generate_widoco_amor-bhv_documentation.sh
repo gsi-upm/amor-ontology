9af79d3f8f05c77459dc2bc55e6740d2062417d1
Álvaro Carrera Barroso <carrerabarroso@gmail.com>
2025-03-19 14:21:30 +0100

    fixing amor-bhv widoco script

--- src/amor/models/bhv/generate_widoco_amor-bhv_documentation.sh
+++ src/amor/models/bhv/generate_widoco_amor-bhv_documentation.sh
@@ -2,7 +2,7 @@ docker run -ti --rm \
     -v ./src/:/usr/local/widoco/in:Z \
     -v ./doc/ontologies:/usr/local/widoco/out:Z \
     ghcr.io/dgarijo/widoco:latest \
-    -confFile in/amor/models/amor/models/bhv/widoco-amor-bhv-config.properties \
+    -confFile in/amor/models/bhv/widoco-amor-bhv-config.properties \
     -ontFile in/amor/models/bhv/amor-bhv.ttl \
     -outFolder out/amor/models/bhv/ns/doc \
     -webVowl -rewriteAll
