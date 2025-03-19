a79637423f9129f532becfaf82728b25bac2ca6a
Álvaro Carrera Barroso <carrerabarroso@gmail.com>
2025-03-19 14:23:08 +0100

    fixing amor-mft widoco script

--- src/amor/models/mft/generate_widoco_amor-mft_documentation.sh
+++ src/amor/models/mft/generate_widoco_amor-mft_documentation.sh
@@ -1,6 +1,6 @@
 docker run -ti --rm \
     -v ./src/:/usr/local/widoco/in:Z \
-    -v ./doc_test/ontologies:/usr/local/widoco/out:Z \
+    -v ./doc/ontologies:/usr/local/widoco/out:Z \
     ghcr.io/dgarijo/widoco:latest \
     -confFile in/amor/models/amor/models/mft/widoco-amor-mft-config.properties \
     -ontFile in/amor/models/mft/amor-mft.ttl \
