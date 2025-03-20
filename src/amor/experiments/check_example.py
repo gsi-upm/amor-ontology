20fd1d4d02e62d8706a6465e17a3b0e271d58d14
Álvaro Carrera Barroso <carrerabarroso@gmail.com>
2025-03-20 09:40:10 +0100

    checking json2ttl conversion

--- src/amor/experiments/check_example.py
+++ src/amor/experiments/check_example.py
@@ -1,17 +1,15 @@
 from rdflib import Graph
 
 g = Graph()
-
 g.parse("examples/amor-experiments-examples.json")
-
-print(g.serialize(format='ttl'))
-
 l = len(g)
 
 g2 = Graph()
 g2.parse('examples/amor-experiments-examples.ttl')
+l2 = len(g2) - 11 # 11 triples are not in the original file (they are the definition of the Ontology: title, preferred prefix, etc.)
 
-l2 = len(g2)
+# g.serialize(destination="graph_from_ttl.ttl", format='ttl')
+# g2.serialize(destination="graph_from_json.ttl", format='ttl')
 
 print(l, l2)
 assert l == l2
