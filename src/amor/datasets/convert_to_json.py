f89c58e74b6b0630e6ee8a8d11152297c3c84f6f
J. Fernando Sánchez <jf.sanchez@upm.es>
2025-06-05 11:35:12 +0200

    datasets: only convert specific str attributes

--- src/amor/datasets/convert_to_json.py
+++ src/amor/datasets/convert_to_json.py
@@ -11,10 +11,8 @@ else:
     raise ValueError("Please provide a TTL file as an argument.")
 
 g.parse(ttl_file, format='turtle')
-#g.parse('./examples/dataset-example.ttl', format='turtle')
 
 
-#with open('../../prefixes.jsonld') as f:
 context_files = [
         '../../prefixes.jsonld',
         '../experiments/amor-experiment-context.jsonld'
@@ -25,7 +23,6 @@ for c in context_files:
     with open(c) as f:
         contexts.append(json.load(f))
 
-#print(g.serialize(format='ttl'))
 js = json.loads(g.serialize(format='json-ld',
                             context=contexts))
 
@@ -42,11 +39,9 @@ dataset = jsonld.frame(js, frame=frame)
 
 # If a dataset has a default language (marked with the schema:language property), strings in this language will be converted to json strings.
 # Strings in other languages, and any string if there is no default, will be converted to an object: {"@value": "<STRING>", "@language": "<lang>"} or {"@value": "<STRING>"} if there is no language tag.
-default_language = dataset.get("schema:language", "und")
-contexts.append({"@language": default_language})
+#default_language = dataset.get("schema:language", "und")
+#contexts.append({"@language": default_language})
 
-#print(json.dumps(js, indent=2))
-#sys.exit(1)
 news_frame = {
         "@context": contexts,
         "@type": "schema:NewsArticle",
@@ -54,13 +49,15 @@ news_frame = {
         "annotations": {}
         }
 
-all_news = {news['@id']: news for news in jsonld.frame(js, frame=news_frame)['@graph']}
-
-
-for news in all_news.values():
-#    print(news)
+def format_news(news):
+    # String attributes that should be converted to json objects
+    str_attrs = ['schema:articleBody', 'schema:headline']
+    for attr in str_attrs:
+        value = news[attr]
+        if isinstance(value, str):
+            news[attr] = {"@value": value}
     if 'annotations' not in news:
-        continue
+        return news
     annotations = news.pop("annotations")
     indexed = {}
     for ann in annotations:
@@ -73,27 +70,18 @@ for news in all_news.values():
             if k == 'amor:Recommendation':
                 ann['oa:hasBody'] = ann['oa:hasBody']['@id']
             indexed[k].append(ann)
-    news["annotations"] = indexed
+    return news
 
-
+all_news = {news['@id']: format_news(news) for news in jsonld.frame(js, frame=news_frame)['@graph']}
 
 parts = dataset["schema:hasPart"]
 
 for k in parts.keys():
     parts[k] = all_news[k]
-#js["schema:hasPart"] = all_news
 
 
 del dataset['@context']
 
 dataset['@context'] = "http://www.gsi.upm.es/ontologies/amor/experiments/context.jsonld"
 
-#print(json.dumps(js, indent=2))
 print(json.dumps(dataset, indent=2))
-
-#del context["parts"]["@container"]
-#with open('frame.json') as f:
-#    frame = json.load(f)
-#frame["@context"] = [
-#        context,
-#        {"@base": "http://www.github.com/gsi-upm/amor-experiment-api#"}]
